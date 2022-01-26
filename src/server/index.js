f4f404f5ec3873d18d2a2e0de65bce7f4a2d2306
Zak Deyneka <zak@info-era.com>
2022-01-26 13:57:44 +0100

    refactoring api

--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,3 @@
-
-// import http from 'http'
-// import app from './server'
-
-// const server = http.createServer(app)
-// let currentApp = app
-// server.listen(3000)
-
-// if (module.hot) {
-//  module.hot.accept('./server', () => {
-//   server.removeListener('request', currentApp)
-//   server.on('request', app)
-//   currentApp = app
-//  })
-// }
-
-// if (module.hot) {
-//     module.hot.accept('./server', function () {
-//         console.log('Accepting the updated server module!');
-//     })
-// }
-
 import app from './server'
 
 const port = 3000
